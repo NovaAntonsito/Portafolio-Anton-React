bf3be4ad11d6335c8d2c532c0baf34a8e7e312eb
Marcos Anton <104325848+NovaAntonsito@users.noreply.github.com>
2025-06-23 15:02:02 -0300

    Cyberpunk Responsive Portfolio Redesign

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -10,40 +10,46 @@ export default function Home() {
     const nameArray = [' ','M','a','r','c','o','s',' ','A','n','t','o','n']
     const jobArray = ['W','e','b',' ','D','e','v','e','l','o','p','e','r']
     
-
-   /* useEffect(()=>{
-      return setTimeout(()=>{
-        setLetterClass('Animation-hover')
-        console.log(setLetterClass);
-      }, 4000)
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLetterClass('Animation-hover')
+        }, 4000)
+        
+        return () => clearTimeout(timer)
     }, [])
-*/
 
-  return (
-    <>
-    <div className='container home-page'>
-        <div className='text-zone'>
-          <h1>
-            <span className={letterClass}>H</span>
-            <span className={`${letterClass} _12`}>o</span>
-            <span className={`${letterClass} _13`}>l</span>
-            <span className={`${letterClass} _14`}>a</span>
-            <span className={`${letterClass} _15`}>,</span>
-            <br/>
-            <span className={`${letterClass} _16`}>s</span>
-            <span className={`${letterClass} _17`}>o</span>
-            <span className={`${letterClass} _18`}>y</span>
-            <img src={LogoDev} alt='developer' className='dev'/>
-             <Animation letterClass = {letterClass} strArray={nameArray} index={19} />
-            <br/>
-            <Animation letterClass = {letterClass} strArray={jobArray} index={22} />
-            </h1>
-            <h2>Desarrollador Web Full Stack</h2>
-            <h2>Desarrollador Back-End / Experto en JS y Java</h2>
-            <Link to = "/contactme" className='flat-botton'>Contacta conmigo!</Link>
-        </div>
-    </div>
-    <Loader type="line-scale"/>
-    </>
-  )
+    return (
+        <>
+            <div className='container home-page'>
+                <div className='cyber-float'></div>
+                <div className='cyber-float'></div>
+                <div className='cyber-float'></div>
+                
+                <div className='text-zone'>
+                    <h1>
+                        <span className={letterClass}>H</span>
+                        <span className={`${letterClass} _12`}>o</span>
+                        <span className={`${letterClass} _13`}>l</span>
+                        <span className={`${letterClass} _14`}>a</span>
+                        <span className={`${letterClass} _15`}>,</span>
+                        <br/>
+                        <span className={`${letterClass} _16`}>s</span>
+                        <span className={`${letterClass} _17`}>o</span>
+                        <span className={`${letterClass} _18`}>y</span>
+                        <div className="glitch-text">
+                            <Animation letterClass={letterClass} strArray={nameArray} index={19} />
+                        </div>
+                        <br/>
+                        <Animation letterClass={letterClass} strArray={jobArray} index={22} />
+                    </h1>
+                    <h2>Desarrollador Web Full Stack</h2>
+                    <h2>Especialista en Back-End / Experto en JS y Java</h2>
+                    <Link to="/contactme" className='flat-botton'>Contacta conmigo!</Link>
+                </div>
+                
+                <img src={LogoDev} alt='developer' className='dev'/>
+            </div>
+            <Loader type="line-scale"/>
+        </>
+    )
 }
--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -3,22 +3,25 @@ import Sidebar from '../Sidebar/Sidebar'
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
-const Layout = () =>{
+const Layout = () => {
     return (
-        <div className='App'>
-            <Sidebar/>
-            <div className='page'>
-                <span className='tags top-tags'>&lt;body&gt;</span>
-
-                <Outlet />
-
-                <span className='tags bottom-tags'>
-                &lt;body&gt;
-                <br />
-                <span className='bottom-tag-html'>&lt;/html&gt;</span>
-                </span>
+        <>
+            <div className="cyberpunk-grid"></div>
+            <div className='App'>
+                <Sidebar/>
+                <div className='page'>
+                    <span className='tags top-tags'>&lt;body&gt;</span>
+                    
+                    <Outlet />
+                    
+                    <span className='tags bottom-tags'>
+                        &lt;/body&gt;
+                        <br />
+                        <span className='bottom-tag-html'>&lt;/html&gt;</span>
+                    </span>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
